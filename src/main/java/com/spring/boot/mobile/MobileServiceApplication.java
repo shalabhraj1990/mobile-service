a3050a726d93173e82c1d41ab768ff434d0253e1
shalabhraj1990 <shalabhrajetc@gmail.com>
2021-12-19 07:23:14 +0530

    entityScan added

--- src/main/java/com/spring/boot/mobile/MobileServiceApplication.java
+++ src/main/java/com/spring/boot/mobile/MobileServiceApplication.java
@@ -2,6 +2,7 @@ package com.spring.boot.mobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.Components;
@@ -9,6 +10,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
+//add below line if the entity is present in different package or different project
+//as by default spring JPA will unable to find the enity while connecting to database
+@EntityScan("msk.spring.boot.common.mobile.entity")
 public class MobileServiceApplication {
 
 	public static void main(String[] args) {
